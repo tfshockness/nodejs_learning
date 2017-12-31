8a3bad8cf2b0f4e3460d31170dea6bc4317805cd
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2017-12-30 18:51:23 -0800

    Adding ENV variables and setting config for heroku

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 var express = require('express');
 var app = express();
-
+var config = require('./src/lib/config');
+var home = require('./src/controller/homeController');
 //IF express is going to point to a public folder to host a page, the function express.static 
 //have to be set as a middleware for express.
 //To add a middleware use use()
 
 //app.use(express.static(__dirname + '/public')); //This will start express server looking for a /public to host
 
-app.get('/', (req, res) => {
-    res.send({
-        A00972539: {
-            name:"Timoteo Shockness",
-            course: "COMPUTER SYSTEM"
-        }
-    })
-});
-
-app.listen(3001, "localhost");
+app.get('/api/home', home.get);
+app.listen(config.port, () => console.log(`Server running on port ${config.port}.`));
---
+++ src/controller/homeController.js
@@ -0,0 +1,10 @@
+module.exports = {
+    get: (req, res) => {
+        res.send({
+            A00972539: {
+                name:"Timoteo Shockness",
+                course: "COMPUTER SYSTEM"
+            }
+        });
+    }
+}
