688028257cf112ad9afb0cbcb1814568f57f8ec0
Timoteo Farias Shockness <timoteo.ppt@gmail.com>
2018-01-02 00:00:33 -0800

    adding remote MongoDB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('../lib/config');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.db);
+mongoose.connect("config.db");
 
 module.exports = {
     mongoose
